807beb1ecb437e0ff43af7a5b2bcf708e6e0f89e
Rafael Vastag <rgvastag@gmail.com>
2022-06-24 11:33:02 -0300

    first controller endpoint

--- src/main/java/com/vastag/sb/controllers/CategoriaController.java
+++ src/main/java/com/vastag/sb/controllers/CategoriaController.java
@@ -1,16 +1,28 @@
 package com.vastag.sb.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping(value = "/categories")
-public class CategoriaController {
+import com.vastag.sb.domain.Categoria;
 
-	@GetMapping(value = "/test")
-	public String getMethod() {
-		return "Running";
-	}
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaController {
 	
+	@GetMapping
+	public List<Categoria> listarCategorias() {
+
+		Categoria c1 = new Categoria(1, "Informática");
+		Categoria c2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> ls = new ArrayList<>();
+		ls.add(c1);
+		ls.add(c2);
+		
+		return ls;
+	}
 }
---
+++ src/main/java/com/vastag/sb/domain/Categoria.java
@@ -0,0 +1,17 @@
+package com.vastag.sb.domain;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Categoria implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+}
