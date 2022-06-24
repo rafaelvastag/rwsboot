12d6c7b77f6479886bc97e32d3fbe2ae1f13d496
Rafael Vastag <rgvastag@gmail.com>
2022-06-24 11:14:34 -0300

    initial

---
+++ src/main/java/com/vastag/sb/RwsbootApplication.java
@@ -0,0 +1,13 @@
+package com.vastag.sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RwsbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RwsbootApplication.class, args);
+	}
+
+}
